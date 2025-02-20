004f9ba7a071543e8d1858e6c4c2771acc4d8e8d
jkouhai <abc@gmail.com>
2025-02-20 04:22:05 -0500

    task3

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,7 +6,7 @@ import {useDispatch} from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const dispatch = useDispatch();
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -249,6 +249,7 @@ const handlePlantsClick = (e) => {
     setShowCart(false);
   };
    const [addToCart, setAddedToCart] = useState({});
+   
    const handleAddToCart = (product) => {
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
